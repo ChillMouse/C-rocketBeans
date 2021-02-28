f5eff47caf5802afe64bd5471a681a3569620356
chillmouse <koftun251201@inbox.ru>
2021-02-28 22:43:33 +0700

    Первые три задания готовы

---
+++ C-rocketBeans/cmake-build-debug/CMakeFiles/C-rocketBeans.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C-rocketBeans.exe"
+  "C-rocketBeans.exe.manifest"
+  "C-rocketBeans.pdb"
+  "CMakeFiles/C-rocketBeans.dir/event1.c.obj"
+  "libC-rocketBeans.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-rocketBeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C-rocketBeans/cmake-build-debug/CMakeFiles/Projects_C_and_Cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Projects_C_and_Cpp.
+# This may be replaced when dependencies are built.
