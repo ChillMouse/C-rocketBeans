d39420eaf2295ea56334634c562ccc25c0fe6e40
Denis Kovtunenko <rorkopxrnrxv@mail.ru>
2021-03-03 22:28:16 +0700

    Update event1.c
    arrayWithIntegers - переименовал массив.
    addit_A - переименовал переменные, потому что просто переменные A, B, C, D уже были объявлены.

--- C-rocketBeans/event1.c
+++ C-rocketBeans/event1.c
@@ -1,5 +1,5 @@
 #include "phoenixLib.h"
-#include "Lib1.h"
+#include "additionalVariables.h"
 
 static int a = 12, b = 8, c = 34, d = 123;
 
@@ -42,31 +42,31 @@ int event1_p3() {
 
 int event1_p5(){
     printf("Введите значения переменных A, B, C, D:");
-    scanf("%d %d %d %d", &A, &B, &C, &D); // Считывание значений из консоли, которые мы вводим.
+    scanf("%d %d %d %d", &addit_A, &addit_B, &addit_C, &addit_D); // Считывание значений из консоли, которые мы вводим.
 
-    A += B + 1 + C + 1;
-    A %= D;
-    B /= 6;
-    C += B - 1 + A;
-    A++;
-    D = (B >> 1) & (C >> 1); // Арифметические операции.
+    addit_A += addit_B + 1 + addit_C + 1;
+    addit_A %= addit_D;
+    addit_B /= 6;
+    addit_C += addit_B - 1 + addit_A;
+    addit_A++;
+    addit_D = getAfterBinary(*&addit_B, *&addit_C); // Арифметические операции.
 
     printf(" A: %d; B: %d; C: %o; D: %o; Event1_p4\n", A, B, C, D); // Вывод
 
     return 0;
 }
 int event1_p6(){
-    int mass[]={12,8,34,123};
-    printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", mass[0], mass[1], mass[2], mass[3]);
+    int arrayWithIntegers[] = {12, 8, 34, 123};
+    printf(" arrayWithIntegers[0]: %d; arrayWithIntegers[1]: %d; arrayWithIntegers[2]: %o; arrayWithIntegers[3]: %o; Event1_p5\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]);
 
-    mass[0] += mass[1] + 1 + mass[2] + 1;
-    mass[0] %= mass[3];
-    mass[1] /= 6;
-    mass[2] += mass[1] - 1 + mass[0];
-    mass[0]++;
-    mass[3] = (mass[1] >> 1) & (mass[2] >> 1); // Арифметические операции.
+    arrayWithIntegers[0] += arrayWithIntegers[1] + 1 + arrayWithIntegers[2] + 1;
+    arrayWithIntegers[0] %= arrayWithIntegers[3];
+    arrayWithIntegers[1] /= 6;
+    arrayWithIntegers[2] += arrayWithIntegers[1] - 1 + arrayWithIntegers[0];
+    arrayWithIntegers[0]++;
+    arrayWithIntegers[3] = (arrayWithIntegers[1] >> 1) & (arrayWithIntegers[2] >> 1); // Арифметические операции.
 
-    printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", mass[0], mass[1], mass[2], mass[3]); // Вывод
+    printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]); // Вывод
 
     return 0;
 }
