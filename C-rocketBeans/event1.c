ac85486f8bfa97a41a432e9e50425557c2a9aad4
chillmouse <koftun251201@inbox.ru>
2021-03-03 23:15:52 +0700

    Обновил некоторые недочёты: -Переименовал переменные -Новая версия стандарта Си -Указатели добавил -Изменил printf() -Добавил удалённый main(){} 0_o

--- C-rocketBeans/event1.c
+++ C-rocketBeans/event1.c
@@ -1,4 +1,4 @@
-#include "../../phoenixLib.h/phoenixLib.h/phoenixLib.h"
+#include "../../phoenixLib.h/phoenixLib.h"
 #include "additionalVariables.h"
 
 static int a = 12, b = 8, c = 34, d = 123;
@@ -8,6 +8,28 @@ int event1_p3();
 int event1_p5();
 int event1_p6();
 int getAfterBinary();
+
+int main(){
+    translateOutput(); // Смена кодировки вывода консоли.
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p1\n\n", *&a, *&b, *&c, *&d); // Вывод исходных данных
+
+    a += b + 1 + c + 1;
+    a %= d;
+    b /= 6;
+    c += b - 1 + a;
+    a++;
+    d = getAfterBinary(*&b, *&c); // Арифметические преобразования с обращением по указателю.
+
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p1\n\n", *&a, *&b, *&c, *&d); // Вывод преобразований
+
+    a = 0; b = 0; c = 0; d = 0;
+    event1_p2(); // Задача 2. Денис
+    event1_p3(); // Задача 3. Денис
+                 // Задача 4. Геля
+    event1_p5(); // Задача 5. Макс
+    event1_p6(); // Задача 6. Макс
+    return 0;
+}
 
 int event1_p2(){
     int A = 512, B = 1024, C = 2048, D = 4096; // Локальные переменные.
@@ -26,7 +48,7 @@ int event1_p2(){
 int event1_p3() {
     int A, B, C, D;
     printf("Введите значения переменных A, B, C, D:");
-    scanf("%d %d %d %d", &A, &B, &C, &D); // Считывание значений из консоли, которые мы вводим.
+    scanf("%d,%d,%d,%d", &A, &B, &C, &D); // Считывание значений из консоли, которые мы вводим.
 
     A += B + 1 + C + 1;
     A %= D;
@@ -35,7 +57,7 @@ int event1_p3() {
     A++;
     D = getAfterBinary(*&B, *&C); // Арифметические операции.
 
-    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p3\n\n", A, B, C, D); // Вывод
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p3\n\n", *&A, *&B, *&C, *&D); // Вывод
 
     return 0;
 }
@@ -51,13 +73,13 @@ int event1_p5(){
     addit_A++;
     addit_D = getAfterBinary(*&addit_B, *&addit_C); // Арифметические операции.
 
-    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p4\n", *&addit_A, *&addit_B, *&addit_C, *&addit_D); // Вывод
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p4\n\n", *&addit_A, *&addit_B, *&addit_C, *&addit_D); // Вывод
 
     return 0;
 }
 int event1_p6(){
     int arrayWithIntegers[] = {12, 8, 34, 123};
-    printf(" arrayWithIntegers[0]: %d; arrayWithIntegers[1]: %d; arrayWithIntegers[2]: %o; arrayWithIntegers[3]: %o; Event1_p5\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]);
+    printf(" arrayWithIntegers[0]: %d; arrayWithIntegers[1]: %d; arrayWithIntegers[2]: %o; arrayWithIntegers[3]: %o; Event1_p5\n\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]);
 
     arrayWithIntegers[0] += arrayWithIntegers[1] + 1 + arrayWithIntegers[2] + 1;
     arrayWithIntegers[0] %= arrayWithIntegers[3];
@@ -66,7 +88,7 @@ int event1_p6(){
     arrayWithIntegers[0]++;
     arrayWithIntegers[3] = (arrayWithIntegers[1] >> 1) & (arrayWithIntegers[2] >> 1); // Арифметические операции.
 
-    printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]); // Вывод
+    printf(" arrayWithIntegers[0]: %d; arrayWithIntegers[1]: %d; arrayWithIntegers[2]: %o; arrayWithIntegers[3]: %o; Event1_p5\n\n", arrayWithIntegers[0], arrayWithIntegers[1], arrayWithIntegers[2], arrayWithIntegers[3]); // Вывод
 
     return 0;
 }
