9f1c54ff2d33239f2a6a0696a602c622d5f2e519
Denis Kovtunenko <rorkopxrnrxv@mail.ru>
2021-03-03 22:14:37 +0700

    Слияние двух файлов
    Спасибо, что стёрли мои файлы :(

--- C-rocketBeans/event1.c
+++ C-rocketBeans/event1.c
@@ -5,49 +5,25 @@ static int a = 12, b = 8, c = 34, d = 123;
 
 int event1_p2();
 int event1_p3();
-
-int event1_p4();
-
 int event1_p5();
-
-int main(){
-    translateOutput(); // Смена кодировки вывода консоли.
-    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p1\n", a, b, c, d); // Вывод исходных данных
-
-    a += b + 1 + c + 1;
-    a %= d;
-    b /= 6;
-    c += b - 1 + a;
-    a++;
-    d = (b >> 1) & (c >> 1); // Арифметические преобразования
-
-   printf(" A: %d; B: %d; C: %o; D: %o; Event1_p1\n", a, b, c, d); // Вывод преобразований
-
-   a = 0; b = 0; c = 0; d = 0;
-   event1_p2(); // Задача 2.
-   event1_p3(); // Задача 3.
-   event1_p4(); // Задача 4.
-   event1_p5(); // Задача 5.
-   return 0;
-}
-
-
+int event1_p6();
+int getAfterBinary();
 
 int event1_p2(){
-        int A = 512, B = 1024, C = 2048, D = 4096; // Локальные переменные.
-        A += B + 1 + C + 1;
-        A %= D;
-        B /= 6;
-        C += B - 1 + A;
-        A++;
-        D = (B >> 1) & (C >> 1); // Арифметические операции.
+    int A = 512, B = 1024, C = 2048, D = 4096; // Локальные переменные.
+    A += B + 1 + C + 1;
+    A %= D;
+    B /= 6;
+    C += B - 1 + A;
+    A++;
+    D = getAfterBinary(*&B, *&C); // Арифметические операции.
 
-        printf(" A: %d; B: %d; C: %o; D: %o; Event1_p2\n", A, B, C, D); // Вывод
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p2\n\n", A, B, C, D); // Вывод
 
-        return 0;
+    return 0;
 }
 
-int event1_p3(){
+int event1_p3() {
     int A, B, C, D;
     printf("Введите значения переменных A, B, C, D:");
     scanf("%d %d %d %d", &A, &B, &C, &D); // Считывание значений из консоли, которые мы вводим.
@@ -57,13 +33,14 @@ int event1_p3(){
     B /= 6;
     C += B - 1 + A;
     A++;
-    D = (B >> 1) & (C >> 1); // Арифметические операции.
+    D = getAfterBinary(*&B, *&C); // Арифметические операции.
 
-    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p3\n", A, B, C, D); // Вывод
+    printf(" A: %d; B: %d; C: %o; D: %o; Event1_p3\n\n", A, B, C, D); // Вывод
 
     return 0;
 }
-int event1_p4(){
+
+int event1_p5(){
     printf("Введите значения переменных A, B, C, D:");
     scanf("%d %d %d %d", &A, &B, &C, &D); // Считывание значений из консоли, которые мы вводим.
 
@@ -78,7 +55,7 @@ int event1_p4(){
 
     return 0;
 }
-int event1_p5(){
+int event1_p6(){
     int mass[]={12,8,34,123};
     printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", mass[0], mass[1], mass[2], mass[3]);
 
@@ -92,4 +69,8 @@ int event1_p5(){
     printf(" mass[0]: %d; mass[1]: %d; mass[2]: %o; mass[3]: %o; Event1_p5\n", mass[0], mass[1], mass[2], mass[3]); // Вывод
 
     return 0;
+}
+
+int getAfterBinary(int B, int C){
+   return ((B >> 1) & 126) & ((C >> 1) & 7);
 }
